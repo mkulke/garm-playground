1e467c3e07ff53aeea09e3554b4cee604b915d86
Magnus Kulke <magnuskulke@microsoft.com>
2023-05-04 12:16:03 +0200

    Add docker build+push wf
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- init.sh
+++ init.sh
@@ -11,8 +11,6 @@ envsubst < /etc/garm/config.toml.tmpl > /etc/garm/config.toml
 
 : ${SUBSCRIPTION_ID:?"required"}
 : ${AZURE_CLIENT_ID:?"required"}
-: ${AZURE_TENANT_ID:?"required"}
-: ${AZURE_CLIENT_SECRET:?"required"}
 
 envsubst < /etc/garm/azure-config.toml.tmpl > /etc/garm/azure-config.toml
 
